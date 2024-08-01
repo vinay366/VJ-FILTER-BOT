3522ec42f26a70f707c2ba7fbd6a5ce9ee560d4d
sourav0789 <souravbosu3487@gmail.com>
2024-08-01 18:59:41 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -163,10 +163,10 @@ async def start(client, message):
         )
         return
     data = message.command[1]
-    if data.split("-", 1)[0] == "VJ":
+    if data.split("-", 1)[0] == "SB":
         user_id = int(data.split("-", 1)[1])
-        vj = await referal_add_user(user_id, message.from_user.id)
-        if vj and PREMIUM_AND_REFERAL_MODE == True:
+        sb = await referal_add_user(user_id, message.from_user.id)
+        if sb and PREMIUM_AND_REFERAL_MODE == True:
             await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
             num_referrals = await get_referal_users_count(user_id)
             await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))

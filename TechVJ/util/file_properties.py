531b3dad6b72af4b1bd7d1a24a3673e8e8a9b2b3
UHD4K <148746604+UHD4k@users.noreply.github.com>
2024-11-21 22:22:49 +0530

    Update file_properties.py

--- TechVJ/util/file_properties.py
+++ TechVJ/util/file_properties.py
@@ -25,7 +25,7 @@ async def get_file_ids(client: Client, chat_id: int, id: int) -> Optional[FileId
     file_id = await parse_file_id(message)
     setattr(file_id, "file_size", getattr(media, "file_size", 0))
     setattr(file_id, "mime_type", getattr(media, "mime_type", ""))
-    setattr(file_id, "file_name", getattr(media, "file_name", ""))
+    setattr(file_id, "caption", getattr(media, "caption", ""))
     setattr(file_id, "unique_id", file_unique_id)
     return file_id
 
@@ -52,7 +52,7 @@ def get_hash(media_msg: Message) -> str:
 
 def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
-    return getattr(media, 'file_name', "")
+    return getattr(media, 'caption', "")
 
 def get_media_file_size(m):
     media = get_media_from_message(m)

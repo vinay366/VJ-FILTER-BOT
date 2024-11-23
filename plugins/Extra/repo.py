c0d90a5a950fb032a856be6bb34e425b22fa7d3e
UHD4K <148746604+UHD4k@users.noreply.github.com>
2024-11-23 14:37:33 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by : @PMIxOfficials</b>
 
 """
         if qw.get("description"):

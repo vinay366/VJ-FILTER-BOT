b28de3e7d9096b944661d19b1d6b3c4c8887b0ed
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-01-21 09:02:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ae41R-c2frc4NTll')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movies_Magic_club')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Siva9789')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))

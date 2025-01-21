24e3d59538fe2e0e1ff3f0a23a14a964d56a034f
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-01-21 09:07:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -115,7 +115,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'onepageyam.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '00eec535be25d87a1d96244502b2d1ec7498f6b0')
-VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
+VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Sr_Movie_Links/46')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))

ee0eb23bd9410234636171ffe1a99a1fff6363a2
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-01-21 08:59:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,8 +88,8 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ae41R-c2frc4NTll')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movies_Magic_club')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
 

c8995fb57062c1a4e8191d172f079cade3e8f1ec
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-01-21 09:05:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'onepageyam.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '00eec535be25d87a1d96244502b2d1ec7498f6b0')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')

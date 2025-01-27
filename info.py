9f6d58b85184aa5ff04f651354e9ae72ee6addf5
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-01-27 19:52:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,8 +126,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onepageyam.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '00eec535be25d87a1d96244502b2d1ec7498f6b0')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '139ebf8c6591acc6a69db83f200f2285874dbdbf')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Sr_Movie_Links/46') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
